b9fda98671c972b626a0dbfd5910b10e53947f97
gauthci <gauthci@gmail.com>
2025-05-16 09:45:22 +0000

    Trendstrat

--- bot2/user_data/hyperopts/hyperopt.py
+++ bot2/user_data/hyperopts/hyperopt.py
@@ -1,22 +1,16 @@
-from freqtrade.optimize.hyperopt import IHyperOpt
-from freqtrade.optimize.space import Categorical, Dimension, Integer, Real
+from freqtrade.optimize.space import Integer, Real
+import talib.abstract as ta
 
-class AggressiveRSIHopt(IHyperOpt):
-    @staticmethod
+def custom_hyperopt():
     def indicator_space():
         return [
-            Integer(20, 40, name='rsi_buy'),
-            Integer(60, 80, name='rsi_sell'),
+            Integer(20, 40, name='buy_rsi'),
+            Integer(60, 80, name='sell_rsi'),
             Real(-0.1, -0.01, name='stoploss'),
             Real(0.01, 0.1, name='trailing_stop_positive'),
             Real(0.01, 0.1, name='trailing_stop_positive_offset'),
         ]
 
-    @staticmethod
-    def sell_indicator_space():
-        return []
-
-    @staticmethod
     def generate_buy_signal(dataframe, metadata, params):
         dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
         boll = ta.BBANDS(dataframe['close'], timeperiod=20)
@@ -25,21 +19,27 @@ class AggressiveRSIHopt(IHyperOpt):
         dataframe['ema_200'] = ta.EMA(dataframe, timeperiod=200)
 
         dataframe.loc[
-            (dataframe['rsi'] < params['rsi_buy']) &
+            (dataframe['rsi'] < params['buy_rsi']) &
             (dataframe['close'] < dataframe['bb_lowerband']) &
             (dataframe['ema_50'] > dataframe['ema_200']),
             'buy'] = 1
         return dataframe
 
-    @staticmethod
     def generate_sell_signal(dataframe, metadata, params):
         dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
         boll = ta.BBANDS(dataframe['close'], timeperiod=20)
         dataframe['bb_upperband'] = boll['upperband']
 
         dataframe.loc[
-            (dataframe['rsi'] > params['rsi_sell']) |
+            (dataframe['rsi'] > params['sell_rsi']) |
             (dataframe['close'] > dataframe['bb_upperband']),
             'sell'] = 1
         return dataframe
+
+    return {
+        "buy": generate_buy_signal,
+        "sell": generate_sell_signal,
+        "indicator_space": indicator_space,
+        "sell_indicator_space": lambda: [],
+    }
 
